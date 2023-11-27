f57e84dc836124f5267fead020a7f0a14745b0c0
clipsey24 <christopher.lipsey@gmail.com>
2023-11-27 10:23:36 +0000

    added layout login and register page and added css

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def score_card():
+def hoem():
   return render_template('home.html')
 
 @app.route('/register')
 def register():
   return render_template('register.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+  return render_template('home.html')
+
+@app.route('/layout')
+def layout():
+  return render_template('layout.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
